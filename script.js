311c7b452277568401338715fb6473a40a55b717
ochiengmanuel554-beep <ochiengmanuel554@gmail.com>
2026-07-12 00:19:51 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("matches").innerHTML = `
-  <h2>⚽ Live Football Section</h2>
-  <p>Your football website is now ready to display live matches.</p>
-  <p>In the next step, we'll connect it to the football API.</p>
-`;
